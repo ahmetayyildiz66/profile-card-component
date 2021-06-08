2fdfd2f58fe6e8f22652ab4bae7de09942938fc4
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-06-08 06:53:09 +0300

    Almost completed desktop versrion

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dark-gray': 'hsl(0, 0%, 59%)'
+      },
+      fontFamily: {
+        'kumbh-sans': ['Kumbh Sans', 'sans-serif']
+      }
+    },
+    screens: {
+      'sm': '375px',
+      'lg': '1440px'
+    },
   },
   variants: {
     extend: {},
